4c809b3cdbb479bc9c6d68438cd27e2a36b1ecce
Felipe Brabes <lipebrabes@gmail.com>
2024-10-31 16:33:30 -0300

    refactor: interface to people.tsx

--- frontend-boilerplate/src/components/people-and-lawyers/people.tsx
+++ frontend-boilerplate/src/components/people-and-lawyers/people.tsx
@@ -1,6 +1,6 @@
 import { Flex,Heading, Text } from "@radix-ui/themes";
-
-export function People({people}) {
+import { RelatedPeople } from "@/interfaces/Lawsuit-details.types";
+export function People({people} : {people: RelatedPeople}) {
   return <Flex align='start' direction={"column"} gap="0">
             <Heading size='2'>{people.name}</Heading>
             <Text size='1'>{"Parte envolvida - "+ people.role}</Text>
