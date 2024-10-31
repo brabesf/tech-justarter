7cc3b79163c3fe577ca47332ef9363c0d95ee956
Felipe Brabes <lipebrabes@gmail.com>
2024-10-31 16:31:45 -0300

    test: lawyer.tsx

---
+++ frontend-boilerplate/src/components/people-and-lawyers/lawyer.spec.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Lawyer } from './lawyer';
+
+describe('<Lawyer />', () => {
+    const exampleLawyer = {
+      name: 'Ana Joyce',
+    };
+  
+    it('should render the lawyer details correctly', () => {
+      render(<Lawyer lawyer={exampleLawyer} />);
+  
+      expect(screen.getByText(exampleLawyer.name)).toBeInTheDocument();
+      expect(screen.getByText('Advogado envolvido')).toBeInTheDocument();
+    });
+  
+  });
